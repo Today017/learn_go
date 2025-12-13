2407bfa3e65e7f52404a2c74cab79ddf4d07908e
Today017 <yesterday998244353@gmail.com>
2025-12-13 18:38:19 +0900

    1-2

---
+++ handlers/handler.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func HelloHandler(w http.ResponseWriter, req *http.Request) {
+	//io.WriteString(w, s)
+	//io.Writer型wに文字列sを書き込む
+	io.WriteString(w, "Hello, world!\n")
+
+	//io.Writer型=インターフェース
+	/*
+		type Write interface {
+			Write(p []byte) (n int, err error)
+		}
+	*/
+	//-> メソッドとしてWrite(int n, error err)->[]byte pを持つならOK
+	//C++のコンセプトみたいなやつ？
+}
+
+func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Article...\n")
+}
+
+func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Article List\n")
+}
+
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	articleID := 1
+	resString := fmt.Sprintf("Article No.%d\n", articleID)
+	io.WriteString(w, resString)
+}
+
+func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Nice...\n")
+}
+
+func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Posting Comment...\n")
+}
+
+
+//main.go->handlers/handler.go
+//関数の定義の仕方変更
+//先頭を大文字
+	//大文字にすることでパッケージ外からも参照可能にする
--- main.go
+++ main.go
@@ -1,40 +1,28 @@
 package main
 
 import (
-    "io"
-    "log"
-    "net/http"
+	"log"
+	"net/http"
+	"github.com/Today017/learn_go/handlers"
 )
 
 func main() {
-    //ハンドラ
-    //HTTPリクエストを受け取って、それに対するレスポンス内容をコネクションに書き込む関数
-    helloHandler := func(w http.ResponseWriter, req* http.Request) {
-        //io.WriteString(w, s)
-        //io.Writer型wに文字列sを書き込む
-        io.WriteString(w, "Hello, world!\n")
+	//http.HandleFuncにハンドラ関数を渡すことで、サーバーでこのハンドラが使われるようになる
+	http.HandleFunc("/hello", handlers.HelloHandler) //パス指定
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	http.HandleFunc("/comment", handlers.PostCommentHandler)
 
-        //io.Writer型=インターフェース
-        /*
-            type Write interface {
-                Write(p []byte) (n int, err error)
-            }
-        */
-        //-> メソッドとしてWrite(int n, error err)->[]byte pを持つならOK
-        //C++のコンセプトみたいなやつ？
-    }
+	//ターミナルへのログ表示
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	//http.ListenAndServe(addr string, handler Handler) error
+	//addr=アドレス
+	//error=サーバー起動時に起こったエラー
+	//errorをlog.Fatelに渡している
+	//log.Fatal=エラーをログ出力して異常終了
 
-    //http.HandleFuncにハンドラ関数を渡すことで、サーバーでこのハンドラが使われるようになる
-    http.HandleFunc("/", helloHandler)
-
-    //ターミナルへのログ表示
-    log.Println("server start at port 8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
-    //http.ListenAndServe(addr string, handler Handler) error
-    //addr=アドレス
-    //error=サーバー起動時に起こったエラー
-    //errorをlog.Fatelに渡している
-    //log.Fatal=エラーをログ出力して異常終了
-
-    //handler=nilは？
+	//handler=nilは？
 }
