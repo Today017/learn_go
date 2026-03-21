259a051a495aa7bdf0bc92be907be6ee5a37079d
Today017 <yesterday998244353@gmail.com>
2026-03-21 15:26:21 +0900

    3-5

--- db/main.go
+++ db/main.go
@@ -29,37 +29,70 @@ func main() {
 	}
 	defer db.Close()
 
-	const sqlStr = `
-		select title, contents, username, nice
-		from articles;
-	`
-	rows, err := db.Query(sqlStr)
-	// rows: sql.Rows
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer rows.Close()
+	{
+		const sqlStr = `
+			select title, contents, username, nice, created_at
+			from articles
+		`
+		// fmtパッケージ：SQLインジェクション対策がされていないので良くない
 
-	articleArray := make([]models.Article, 0)
-	for rows.Next() { // while と同じ感じ？
-		var article models.Article
-		// // 指定した変数ポインタに読み出す
-		// err := rows.Scan(&article.Title, &article.Contents, &article.UserName, &article.NiceNum)
-
-		var createTime sql.NullTime
-		err := rows.Scan(&article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createTime)
-
-		if createTime.Valid {
-			article.CreatedAt = createTime.Time
-		}
-
+		rows, err := db.Query(sqlStr)
+		// rows: sql.Rows
 		if err != nil {
 			fmt.Println(err)
-		} else {
-			articleArray = append(articleArray, article)
+			return
 		}
+		defer rows.Close()
+
+		articleArray := make([]models.Article, 0)
+		for rows.Next() { // while と同じ感じ？
+			var article models.Article
+			// // 指定した変数ポインタに読み出す
+			// err := rows.Scan(&article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+
+			var createdTime sql.NullTime
+			err := rows.Scan(&article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+
+			if createdTime.Valid {
+				article.CreatedAt = createdTime.Time
+			}
+
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				articleArray = append(articleArray, article)
+			}
+		}
+
+		fmt.Printf("%+v\n", articleArray)
+		fmt.Println("==================")
 	}
 
-	fmt.Printf("%+v\n", articleArray)
+	{ // 1行だけ読み出す場合の書き方
+		articleID := 1000
+		const sqlStr = `
+			select *
+			from articles
+			where article_id = ?;
+		`
+		row := db.QueryRow(sqlStr, articleID)
+		if err := row.Err(); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		var article models.Article
+		var createdTime sql.NullTime
+		err = row.Scan(&articleID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if createdTime.Valid {
+			article.CreatedAt = createdTime.Time
+		}
+
+		fmt.Printf("%+v\n", article)
+	}
 }
